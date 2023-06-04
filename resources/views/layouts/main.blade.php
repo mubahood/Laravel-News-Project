7bf13b22fc2b12d387c3b9c6d3c61a4444ebf976
Muhindo Mubaraka <mubs0x@gmail.com>
2023-06-04 17:03:51 +0300

    url set

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <base href="http://127.0.0.1:8000/">
+    <base href="{{ url('') }}/">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/bootstrap.css">
